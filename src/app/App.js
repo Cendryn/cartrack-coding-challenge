2be0a238d02ec3c5f49ded21fcf8cace6c8b18d3
Cendryn <ace.almazar@gmail.com>
2021-02-14 10:48:25 +0800

    get data using fetch API

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const App = () => (
-    <div>APP</div>
-);
+const App = () => {
+  const [data, setData] = useState({ initialData: [], filteredData: [] });
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    setIsFetching(true);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setData({ initialData: data, filteredData: data });
+        setIsFetching(false);
+      });
+  }, []);
+  
+  return (
+    <main>APP</main>
+  );
+}
 
 export default App;
