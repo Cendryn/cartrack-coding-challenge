e8d860d5252207cb023a4c61224a71e14fe0c19b
Cendryn <ace.almazar@gmail.com>
2021-02-14 12:55:59 +0800

    add search and table component

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,27 @@
 import React, { useEffect, useState } from 'react';
 
+import Search from './components/Search/Search.jsx';
+import Table from './components/Table/Table.jsx';
+
+const columns = [
+  {
+    name: 'Name',
+    accessor: 'name',
+  },
+  {
+    name: 'Username',
+    accessor: 'username',
+  },
+  {
+    name: 'Email',
+    accessor: 'email',
+  },
+  {
+    name: 'Contact Number',
+    accessor: 'phone',
+  },
+];
+
 const App = () => {
   const [data, setData] = useState({ initialData: [], filteredData: [] });
   const [isFetching, setIsFetching] = useState(false);
@@ -14,9 +36,25 @@ const App = () => {
         setIsFetching(false);
       });
   }, []);
+
+  const handleSearch = (text) => {
+    const searchText = text.trim().toLowerCase();
+
+    const filteredData = data.initialData.filter(item => item.name.trim().toLowerCase().includes(searchText) || item.email.trim().toLowerCase().includes(searchText))
+    
+    setData({ ...data, filteredData });
+  }
   
   return (
-    <main>APP</main>
+    <main>
+      <Search onSearch={handleSearch} />
+      <Table
+        columns={columns}
+        data={data.filteredData}
+        isFetching={isFetching}
+        hasSearch
+      />
+    </main>
   );
 }
 
